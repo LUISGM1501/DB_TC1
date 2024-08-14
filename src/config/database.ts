08313271d484552c604d6b4ebfb06ad29e72c577
Luis <luisgerardourbsalz@gmail.com>
2024-08-14 17:12:25 -0600

    Proteccion de variables

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'database',  
-  port: parseInt(process.env.DB_PORT || '5432', 10),  
-  username: process.env.DB_USERNAME || 'user', 
-  password: process.env.DB_PASSWORD || 'password', 
-  database: process.env.DB_NAME || 'DB_TC1',  
+  host: process.env.DB_HOST as string,  
+  port: parseInt(process.env.DB_PORT as string),  
+  username: process.env.DB_USERNAME as string, 
+  password: process.env.DB_PASSWORD as string, 
+  database: process.env.DB_NAME as string,  
   entities: [__dirname + '/../models/*.ts'], 
   synchronize: true, 
 });
